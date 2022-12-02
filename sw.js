d22004c84dc79111e76d6b3374ba3fc59d670d5d
Martin Mateo <martin.mateo@davinci.edu.ar>
2022-12-02 17:38:36 -0300

    app

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+const CACHE_NAME = 'cache-1';
 self.addEventListener('install', function(e){
     console.log(e);
-})
+    const cache = caches.open(CACHE_NAME).then( cache => {
+        return cache.addAll([
+            'app.js',
+            'index.html'
+        ]);
+    });
+    e.waitUntil( cache );
+});
