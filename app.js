e50340da9182406e46c4d63a70676e1f1b4c101f
Martin Mateo <martin.mateo@davinci.edu.ar>
2022-12-05 16:16:05 -0300

    app

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const btnSave = document.querySelector('#btn-save');
 const textArea = document.querySelector('#text-1');
 let container = document.querySelector('.collection');
 let lista = [];
-// FORMATO:
-//let lista = [ { nota: 'descripción de la nota', fecha: '17/11/2022'} ];
+let btnsEliminar = [];
+let idnotas = 0
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,26 +16,61 @@ document.addEventListener('DOMContentLoaded', function() {
 
     lista = leerNotas();
     renderizarNotas(lista);
+    idnotas = lista.length;
 });
 
 /* - FUNCION 1:  Obtiene el texto del textArea y guarda en el texto en el array - */
 btnSave.addEventListener('click', ()=>{
-  console.log('Guardando nota');
+  let getFecha = new Date().toLocaleString();
+  let nota = {
+    nota: textArea.value,
+    fecha: getFecha
+  }
+  lista.push(nota);
+  textArea.value = '';
   guardarNotas(lista);
 })
 
 /* -------- FUNCION 2: Recibe el array y lo guarda en el localStorage ------- */
 function guardarNotas(array){
-
+  localStorage.setItem('lista', JSON.stringify(array));
   renderizarNotas();
 }
 
 /* --------- FUNCION 3: Lee los datos del localStorage y lo retorna --------- */
 function leerNotas(){
-
+  return localStorage.getItem('lista') ?
+    JSON.parse(localStorage.getItem('lista')) : [];
 }
 
 /* -------- FUNCION 4: Recibe el array y lo renderiza en el container ------- */
 function renderizarNotas(array){
+  container.innerHTML= '';
 
+  let btnid = 0;
+
+  for (const nota of array) {
+    btnid++;
+
+    let li = document.createElement('li');
+
+    let span = document.createElement('span');
+    span.className = "nota";
+    span.textContent = nota.nota;
+
+    let span2 = document.createElement('span');
+    span2.className = "fecha"
+    span2.textContent = nota.fecha;
+
+    //let tachito = document.createElement("i");
+    //tachito.setAttribute("class", "bi bi-trash float-right btn-delete eliminar");
+    //tachito.setAttribute("id", btnid);
+
+    container.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(span2);
+    //li.appendChild(tachito);
+  };
 }
+
+/* -------- FUNCION 5: Borrar los datos ------- */
--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = 'cache-1';
 self.addEventListener('install', function(e){
-    console.log(e);
     const cache = caches.open(CACHE_NAME).then( cache => {
         return cache.addAll([
             'app.js',
